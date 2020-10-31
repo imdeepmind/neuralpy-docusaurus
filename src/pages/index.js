265f4d36b3762a8de58af0814dd4e8999264cd64
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-10-31 23:30:26 +0530

    changed the text in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,91 +7,77 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
 const features = [
-  {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
+	{
+		title: 'Easy to Use',
+		imageUrl: 'img/affection.svg',
+		description: <>NeuralPy provides an easy interface that is suitable for fast prototyping, learning, and research</>,
+	},
+	{
+		title: 'GPU and CPU',
+		imageUrl: 'img/graphics-card.svg',
+		description: <>As NeuralPy works on top of PyTorch, it can be used on both CPU and GPU very efficiently.</>,
+	},
+	{
+		title: 'Cross-Compatible',
+		imageUrl: 'img/exchange.svg',
+		description: (
+			<>
+				NeuralPy models are cross-compatible with PyTorch model, that mean you can use build the model in PyTorch and
+				train it on PyTorch oe vice-versa.
+			</>
+		),
+	},
 ];
 
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={clsx('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
+function Feature({ imageUrl, title, description }) {
+	const imgUrl = useBaseUrl(imageUrl);
+	return (
+		<div className={clsx('col col--4', styles.feature)}>
+			{imgUrl && (
+				<div className="text--center">
+					<img className={styles.featureImage} src={imgUrl} alt={title} />
+				</div>
+			)}
+			<h3>{title}</h3>
+			<p>{description}</p>
+		</div>
+	);
 }
 
 function Home() {
-  const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link
-              className={clsx(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
-              )}
-              to={useBaseUrl('docs/')}>
-              Get Started
-            </Link>
-          </div>
-        </div>
-      </header>
-      <main>
-        {features && features.length > 0 && (
-          <section className={styles.features}>
-            <div className="container">
-              <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
-              </div>
-            </div>
-          </section>
-        )}
-      </main>
-    </Layout>
-  );
+	const context = useDocusaurusContext();
+	const { siteConfig = {} } = context;
+	return (
+		<Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+			<header className={clsx('hero hero--primary', styles.heroBanner)}>
+				<div className="container">
+					<h1 className="hero__title">{siteConfig.title}</h1>
+					<p className="hero__subtitle">{siteConfig.tagline}</p>
+					<div className={styles.buttons}>
+						<Link
+							className={clsx('button button--outline button--secondary button--lg', styles.getStarted)}
+							to={useBaseUrl('docs/')}
+						>
+							Get Started
+						</Link>
+					</div>
+				</div>
+			</header>
+			<main>
+				{features && features.length > 0 && (
+					<section className={styles.features}>
+						<div className="container">
+							<div className="row">
+								{features.map((props, idx) => (
+									<Feature key={idx} {...props} />
+								))}
+							</div>
+						</div>
+					</section>
+				)}
+			</main>
+		</Layout>
+	);
 }
 
 export default Home;
