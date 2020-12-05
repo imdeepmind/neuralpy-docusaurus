76244d19d1dd6c40445f78fdb4119db48dc38e26
Abhishek Chatterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2020-12-05 19:27:36 +0530

    upgraded to new documentation

--- sidebars.js
+++ sidebars.js
@@ -9,19 +9,6 @@ module.exports = {
 			items: [
 				{
 					type: 'category',
-					label: 'Activation Functions',
-					items: [
-						'docs/activation_functions/gelu',
-						'docs/activation_functions/leaky-relu',
-						'docs/activation_functions/relu',
-						'docs/activation_functions/selu',
-						'docs/activation_functions/sigmoid',
-						'docs/activation_functions/softmax',
-						'docs/activation_functions/tanh',
-					],
-				},
-				{
-					type: 'category',
 					label: 'Callbacks',
 					items: ['docs/callbacks/train-logger'],
 				},
@@ -29,6 +16,19 @@ module.exports = {
 					type: 'category',
 					label: 'Layers',
 					items: [
+						{
+							type: 'category',
+							label: 'Activation Functions',
+							items: [
+								'docs/layers/activation_functions/gelu',
+								'docs/layers/activation_functions/leaky-relu',
+								'docs/layers/activation_functions/relu',
+								'docs/layers/activation_functions/selu',
+								'docs/layers/activation_functions/sigmoid',
+								'docs/layers/activation_functions/softmax',
+								'docs/layers/activation_functions/tanh',
+							],
+						},
 						{
 							type: 'category',
 							label: 'Linear',
@@ -76,6 +76,16 @@ module.exports = {
 								'docs/layers/recurrent/lstmcell',
 							],
 						},
+						{
+							type: 'category',
+							label: 'Regularizers',
+							items: [
+								'docs/layers/regularizers/alpha-dropout',
+								'docs/layers/regularizers/dropout',
+								'docs/layers/regularizers/dropout2d',
+								'docs/layers/regularizers/dropout3d',
+							],
+						},
 					],
 				},
 				{
@@ -101,16 +111,6 @@ module.exports = {
 						'docs/optimizers/rmsprop-optimizer',
 						'docs/optimizers/rprop-optimizer',
 						'docs/optimizers/sgd-optimizer',
-					],
-				},
-				{
-					type: 'category',
-					label: 'Regularizers',
-					items: [
-						'docs/regularizers/alpha-dropout',
-						'docs/regularizers/dropout',
-						'docs/regularizers/dropout2d',
-						'docs/regularizers/dropout3d',
 					],
 				},
 			],
