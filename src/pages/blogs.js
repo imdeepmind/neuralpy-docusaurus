101b593322eea706536b73df7dc89b47f863c8c0
Abhishek Chattterjee (imdeepmind) <abhishekchatterjeejit@gmail.com>
2021-01-26 21:32:03 +0530

    added blog page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -1,21 +1,40 @@
-import React from 'react';
-import Layout from '@theme/Layout';
+import React from "react";
+import { Container, ListGroup, ListGroupItem } from "reactstrap";
+
+import Layout from "@theme/Layout";
 
 function Hello() {
+  const blogs = [];
+
+  const renderBlogs = () => {
+    return (
+      <ListGroup>
+        {blogs.map((val) => {
+          return (
+            <ListGroupItem key={val.url} style={{padding: 10, margin: 5}}>
+              <a href={val.url}>{val.title}</a>
+            </ListGroupItem>
+          );
+        })}
+      </ListGroup>
+    );
+  };
+
+  const renderNoBlogs = () => {
+    return (
+      <ListGroup>
+        <ListGroupItem style={{padding: 10, margin: 5}}>Currently there is no blog</ListGroupItem>
+      </ListGroup>
+    );
+  };
+
   return (
     <Layout title="Hello">
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '50vh',
-          fontSize: '20px',
-        }}>
-        <p>
-          Edit <code>pages/hello.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        {blogs.length > 0 && renderBlogs()}
+
+        {blogs.length < 1 && renderNoBlogs()}
+      </Container>
     </Layout>
   );
 }
